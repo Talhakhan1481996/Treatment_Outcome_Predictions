799fe8f28dc700dd590de2298e13e3297eff642c
mebi-ali <mehboob.ali@xloopdigital>
2023-06-21 11:32:53 +0500

    load phase of etl

--- src/etl_service/load.py
+++ src/etl_service/load.py
@@ -1,14 +1,16 @@
-import redis
 import json
+
 from base_logger import logger
-from transform import transformations 
 from redis_connector import redis_client
-​
+from transform import data_transformations
+
+
 def load_data_to_redis(specializations_dfs: dict) -> dict:
     for key, val in specializations_dfs.items():
         redis_client.set(key, json.dumps(val, indent=2))
-    logger.info('Data Stored in Redis.')
+    logger.info("Data Stored in Redis.")
     return specializations_dfs
-​
+
+
 if __name__ == "__main__":
-    load_data_to_redis(transformations())
+    load_data_to_redis(data_transformations())
