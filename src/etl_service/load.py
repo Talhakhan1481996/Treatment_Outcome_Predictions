c0074e03027d31f7ce5f07af80f41cb1bc3077c6
Talha-Khan-2303-009-KHI-DEG <talha.khan@xloopdigital.com>
2023-06-21 12:12:50 +0500

    load_phase_of_etl_service

--- src/etl_service/load.py
+++ src/etl_service/load.py
@@ -6,6 +6,23 @@ from transform import data_transformations
 
 
 def load_data_to_redis(specializations_dfs: dict) -> dict:
+    """
+    Function: load_data_to_redis
+
+    Description:
+    This function loads data from a dictionary of specializations dataframes into Redis.
+
+    Parameters:
+
+    specializations_dfs (dict): A dictionary containing specializations (key) dataframes (value).
+    Returns:
+
+    dict: The same input dictionary of specializations dataframes.
+
+    Note:
+    The function requires a Redis client to be initialized before calling it.
+    The logger object is expected to be available to log the information regarding the successful storage of data in Redis.
+    """
     for key, val in specializations_dfs.items():
         redis_client.set(key, json.dumps(val, indent=2))
     logger.info("Data Stored in Redis.")
