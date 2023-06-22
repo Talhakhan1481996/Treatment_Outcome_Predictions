8e44de7b7672e1554fc03960d63143c11fde5f5f
Mehboob Ali <118427945+mebi-ali@users.noreply.github.com>
2023-06-22 20:56:21 +0500

    Update src/etl_service/redis_connector.py
    Co-authored-by: gjurdzinski <gjurdzinski@users.noreply.github.com>

--- src/etl_service/redis_connector.py
+++ src/etl_service/redis_connector.py
@@ -5,7 +5,7 @@ def get_redis_client() -> redis.client.Redis:
     redis_host = "localhost"
     redis_port = 6379
     redis_db = 0
-    redis_client = redis.Redis(host=redis_host, port=redis_port, db=redis_db)
+    redis_client = redis.Redis(host="localhost", port=6379, db=0)
     return redis_client
 
 
